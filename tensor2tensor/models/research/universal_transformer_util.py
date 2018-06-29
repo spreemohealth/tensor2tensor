1bc5653ace6c6c6ffda20b3f770aaa7be5083c0d
greg <greg.dubbin@spreemo.com>
2018-06-29 10:46:39 -0400

    Modalities derived from ClassLabelModality are considered class labels. RTransformerEncoder is derived from TransformerEncoder rather than Transformer Transformer and T2TModel check for the decode_hparam "eos_id" to override the default of 1
    Signed-off-by: greg <greg.dubbin@spreemo.com>

--- tensor2tensor/models/research/universal_transformer_util.py
+++ tensor2tensor/models/research/universal_transformer_util.py
@@ -250,38 +250,8 @@ def universal_transformer_layer(x,
       ut_function, initializer = get_ut_layer(x, hparams, ffn_unit,
                                               attention_unit, pad_remover)
 
-<<<<<<< HEAD:tensor2tensor/models/research/universal_transformer_util.py
       output, _, extra_output = tf.foldl(
-<<<<<<< HEAD
-          ut_function, tf.range(hparams.num_rec_steps),
-          initializer=initializer)
-=======
           ut_function, tf.range(hparams.num_rec_steps), initializer=initializer)
-=======
-      # def unstack_input_fn(layer_inputs,step):
-      #   layer_inputs = tf.tuple(tf.unstack(layer_inputs))
-      #   tuple_outputs = rt_function(layer_inputs,step)
-      #   return tf.stack(tuple_outputs)
-
-      # output, _, extra_output = tf.unstack(tf.foldl(
-      #           unstack_input_fn, tf.range(hparams.num_rec_steps), initializer=initializer))
-
-      # output_flatten = lambda x:nest.flatten(x)
-      # def output_pack(x):
-      #   return nest.pack_sequence_as(initializer,x)
-
-      # initializer_flat = output_flatten(initializer)
-
-      # def compute_fn(layer_inputs,step):
-      #   layer_inputs = output_pack(layer_inputs)
-      #   packed_out = rt_function(layer_inputs,step)
-      #   return output_flatten(packed_out)
-
-      # output, _, extra_output = output_pack(tf.foldl(
-      #           compute_fn, tf.range(hparams.num_rec_steps), initializer=initializer))
-
-      # output, _, extra_output = tf.foldl(
-      #           rt_function,tf.range(hparams.num_rec_steps),initializer=initializer)
 
       output, _, extra_output = [res[-1] 
                                   for res in 
@@ -289,8 +259,11 @@ def universal_transformer_layer(x,
                                     rt_function,tf.range(hparams.num_rec_steps),
                                       initializer=initializer)
                                 ]
+<<<<<<< HEAD
 >>>>>>> Modalities derived from ClassLabelModality are:tensor2tensor/models/research/r_transformer_util.py
 >>>>>>> 8b20c2b6... Modalities derived from ClassLabelModality are
+=======
+>>>>>>> b36fc052... Modalities derived from ClassLabelModality are
 
       # Right now, this is only possible when the transition function is an lstm
       if (hparams.recurrence_type == "lstm" and
